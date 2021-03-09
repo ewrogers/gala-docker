0215f7b70372a25f67a6d1cd06a2377ca6aaf065
Erik Rogers <erik.rogers@live.com>
2021-03-09 17:27:29 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Starting Gala node #{NODE_SPECIFIER}... (email=${GALA_EMAIL})"
+echo "Starting Gala node ${NODE_SPECIFIER}... (email=${GALA_EMAIL})"
 exec gala-headless-node email=$GALA_EMAIL password=$GALA_PASSWORD specifier=$NODE_SPECIFIER
